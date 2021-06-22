96045d52525b7957f15cb5e8396c9bcbc96e4a76
Gustin <florian.gustin.business@gmail.com>
2021-06-22 10:54:03 +0200

    fix marker

--- google_maps_implement_main/lib/architecture/presentation/platform/web.dart
+++ google_maps_implement_main/lib/architecture/presentation/platform/web.dart
@@ -16,7 +16,7 @@ class _WebState extends State<Web> {
   late GoogleMapController _googleMapController;
   late FloatingSearchBarController _floatingSearchBarController;
 
-  CameraPosition _cameraPosition = CameraPosition(target: LatLng(28.385233, -81.563873), zoom: 10);
+  CameraPosition _cameraPosition = CameraPosition(target: LatLng(28.385233, -81.563873), zoom: 15);
 
   Geocoding _geocoding = Geocoding();
   Places _places = Places();
@@ -38,6 +38,15 @@ class _WebState extends State<Web> {
     super.dispose();
   }
 
+
+  _updatePosition(latLng){
+    setState(() {
+      _latLng = latLng;
+      _geocoding.findAddress(latLng.latitude, latLng.longitude).then((value) => _address = value!);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -50,17 +59,14 @@ class _WebState extends State<Web> {
             });
           },
           mapType: MapType.normal,
-          onTap: (latLng) async{
-            _address = (await _geocoding.findAddress(latLng.latitude, latLng.longitude))!;
-            // TODO : find a way to move the marker only when navigating;
-          },
+          onTap: _updatePosition,
           markers: {
-            Marker(markerId: MarkerId(_address) , position: _latLng, infoWindow: InfoWindow(title: _address))
+            Marker(markerId: MarkerId(_address) , position: LatLng(_latLng.latitude, _latLng.longitude), infoWindow: InfoWindow(title: _address))
           },
         ),
         FloatingSearchBarWidget(
             floatingSearchBarController: _floatingSearchBarController,
-            onQueryChanged: (query) async{
+            onQueryChanged: (query) {
               setState(() {
                 _places.getSuggestions(query).then((value) => _locations = value!);
               });
@@ -78,7 +84,7 @@ class _WebState extends State<Web> {
                         _floatingSearchBarController.close();
                         _latLng = (await _geocoding.getCoordinates(item.address))!;
                         _address = (await  _geocoding.findAddress(_latLng.latitude, _latLng.longitude))!;
-                        _googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(_latLng.latitude, _latLng.longitude), zoom: 15.0)));
+                        _googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: _latLng, zoom: 20.0)));
                       });
                     },
                   );
